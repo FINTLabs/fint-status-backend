ebe6b93e91c1b93aaa02fa36d92e56c96db354ca
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-10-24 12:14:27 +0200

    new uri

--- src/main/java/no/fint/java/fintstatusservicebackend/TjenesteController.java
+++ src/main/java/no/fint/java/fintstatusservicebackend/TjenesteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/status")
 public class TjenesteController {
 
     private final TjenesteRepository tjenesteRepository;
