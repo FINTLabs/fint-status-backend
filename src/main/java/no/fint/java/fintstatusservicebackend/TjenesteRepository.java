145e22c67a525e572d16e48c72076cbd0935e5ef
Egil Ballestad <egil.ballestad@vigoiks.no>
2024-10-24 12:26:56 +0200

    move json to resources

--- src/main/java/no/fint/java/fintstatusservicebackend/TjenesteRepository.java
+++ src/main/java/no/fint/java/fintstatusservicebackend/TjenesteRepository.java
@@ -4,44 +4,41 @@ package no.fint.java.fintstatusservicebackend;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
-import java.io.File;
+import java.io.InputStream;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Getter
 @Slf4j
 @Repository
 public class TjenesteRepository {
 
-    private List<Tjeneste> tjenesteCache = new ArrayList<>();
+    private final List<Tjeneste> tjenesteCache = new ArrayList<>();
 
     @PostConstruct
     private void fillCache() {
         ObjectMapper mapper = new ObjectMapper();
 
-        try{
-            List<PrometheusData> tjenesteDataList = mapper.readValue(
-                    new File("/Users/sander/IdeaProjects/fint-status-service-backend/src/main/java/no/fint/java/fintstatusservicebackend/data/dummyData.json"),
-                    new TypeReference<>(){});
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("data/dummyData.json");
+            List<PrometheusData> tjenesteDataList = mapper.readValue(inputStream, new TypeReference<>() {});
 
             List<Tjeneste> list = tjenesteDataList.stream().map(this::fromPrometheusDataToTjenesteMapper).toList();
             tjenesteCache.addAll(list);
 
-        } catch (Exception e){
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
 
-    public List<Tjeneste> getTjenesteCache() {
-        return tjenesteCache;
-    }
-
     private Tjeneste fromPrometheusDataToTjenesteMapper(PrometheusData prometheusData) {
-       return Tjeneste
+        return Tjeneste
                 .builder()
                 .name(prometheusData.getLabelAppKubernetesIoName())
                 .orgId(prometheusData.getLabelFintlabsNoOrgId())
@@ -49,8 +46,6 @@ public class TjenesteRepository {
                 .sistSjekket(new Timestamp(new Date().getTime()))
                 .build();
     }
-
-
 
 
 }
